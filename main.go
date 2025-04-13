8febd03f9bee35bc4191588723bf977fd7371061
Ekaterina Perminova <katanatroll@yandex.ru>
2025-04-13 20:41:07 +0400

    IntersectionOfArrays

--- main.go
+++ main.go
@@ -2,31 +2,11 @@ package main
 
 import (
 	// "sync"
-	// "exampleGo/Semaphore/lessons"
+	"exampleGo/Semaphore/lessons"
 	"fmt"
 )
 
-func intersection(a, b []int) []int{
-    c := make([]int, 0)   
-    lenA := len(a)
-    lenB := len(b)
-
-    for i:=0; i<=lenA; i++{
-        for j:=0; j<=lenB; j++{
-            if a[i] == b[j]{
-                c = append(c, a[i])
-                break
-            } 
-        }       
-    }
-
-    return c
-}
-
-
-
-
-func main(){
+func main() {
 
 	// semaphore:= lessons.Semaphore{
 	// 	C: make(chan struct{}, 5),
@@ -38,19 +18,14 @@ func main(){
 
 	// res := lessons.Calc()
 	// fmt.Println(res)
-	
+
 	// mas := []int{2, 7, 9 ,10, 45, 6}
 
-	// even, odd := lessons.EvenOdd(mas)
+	//even, odd := lessons.EvenOdd(mas)
 
 	// fmt.Print(even, odd)
+	a := []int{23, 3, 2, 1, 6, 7, 9}
+	b := []int{6, 2, 4, 23, 23, 9}
 
-	    //         0   1  2  3
-		a := []int{23, 3, 2, 1}
-
-		//         0 1  2  3
-		b := []int{6, 2, 4, 23}
-		// [2, 23]
-	
-		fmt.Printf("%v\n", intersection(a, b))
+	fmt.Printf("%v\n", lessons.Intersection(a, b))
 }
